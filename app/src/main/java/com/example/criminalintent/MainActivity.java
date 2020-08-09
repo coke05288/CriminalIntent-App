c7ce2097df575ad8c5d9e06f29895e24d9af1e2e
YoungJun <coke05288@gmail.com>
2020-08-10 03:59:56 +0900

    Chapter 07 | Hosting Fragment

--- app/src/main/java/com/example/criminalintent/MainActivity.java
+++ app/src/main/java/com/example/criminalintent/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.criminalintent;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
