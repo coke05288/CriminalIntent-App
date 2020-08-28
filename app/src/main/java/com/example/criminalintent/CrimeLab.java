98dd9d00bbdd3e3084f04577bf2e86d39d0f81c7
YoungJun <coke05288@gmail.com>
2020-08-29 01:51:25 +0900

    Add remark CrimeLab.java

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -7,16 +7,20 @@ import java.util.List;
 import java.util.UUID;
 
 public class CrimeLab {
+    //private 키워드로 외부 접근 차단.
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
 
+    //외부 접근이 차단된 CrimeLab의 인스턴스를 생성하기 위한 get함수.
     public static CrimeLab get(Context context){
         if(sCrimeLab == null){
             sCrimeLab = new CrimeLab(context);
         }
         return sCrimeLab;
     }
+
+    //생성자
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
         for(int i =0; i< 100; i++){
