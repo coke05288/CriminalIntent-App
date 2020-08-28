9097ab045c2f4444bc6e00e96c868266cb4b0816
YoungJun <coke05288@gmail.com>
2020-08-29 03:35:43 +0900

    close issue #2 | Clear

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.java
@@ -4,13 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.zip.Inflater;
 
 public class CrimeListFragment extends Fragment {
@@ -36,12 +41,36 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder{
-        public TextView mTitleTextView;
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private Crime mCrime;
+
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckBox;
+
+
         public CrimeHolder(View itemView){
             super(itemView);
-            mTitleTextView =(TextView)itemView;
+            itemView.setOnClickListener(this);
+            mTitleTextView =(TextView)itemView.findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView =(TextView)itemView.findViewById(R.id.list_item_crime_date_text_view);
+            mSolvedCheckBox =(CheckBox) itemView.findViewById(R.id.list_item_crime_solved_check_box);
         }
+
+        public void bindCrime(Crime crime){
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            Date dateTemp = mCrime.getDate();
+            DateFormat df = DateFormat.getDateInstance(DateFormat.FULL, Locale.KOREA);
+            mDateTextView.setText(df.format(dateTemp));
+            mSolvedCheckBox.setChecked(mCrime.isSolved());
+        }
+
+        @Override
+        public void onClick(View v){
+            Toast.makeText(getActivity(), mCrime.getTitle() + "선택됨.",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
@@ -52,13 +81,13 @@ public class CrimeListFragment extends Fragment {
         @Override
         public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType){
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            View view = layoutInflater.inflate(R.layout.list_item_crime, parent, false);
             return new CrimeHolder(view);
         }
         @Override
         public void onBindViewHolder(CrimeHolder holder, int position){
             Crime crime = mCrimes.get(position);
-            holder.mTitleTextView.setText(crime.getTitle());
+            holder.bindCrime(crime);
         }
         @Override
         public int getItemCount(){
